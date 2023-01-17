374a343f69ce8ecb309cf9f5929fca58def61d28
Ajith Arjunan <ajithajeesh@gmail.com>
2023-01-17 21:19:51 +0530

    babel config changed

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,16 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
-    plugins: ['react-native-paper/babel'],
-  };
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["babel-preset-expo"],
+//     plugins: ["react-native-reanimated/plugin"],
+//     plugins: ['react-native-paper/babel'],
+//   };
+// };
+
+module.exports = {
+  presets: ["babel-preset-expo"],
+  plugins: [
+    'react-native-paper/babel',
+    "react-native-reanimated/plugin"
+  ],
 };
